3cbd3e54617bfe10392d8e47f20421a720a57cd8
Twan <twanwalpot@gmail.com>
2020-10-20 14:54:44 +0200

    app overview prototype

---
+++ proto.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta, time
+
+mollie = Service(
+    'https://api.mollie.com/v2/',
+    api_key='123',
+)
+
+
+class Payment(Model):
+    resource: str
+    id: str
+    mode: str
+    created_at: datetime
+    status: str
+
+
+@mollie.collector(Payment, interval=timedelta(days=1), start=time(hour=17))
+def daily_payments():
+    last_id = mollie.ctx.get('last_id')
+    payments = mollie.get('payments/', params={'from': last_id})
+    mollie.ctx['last_id'] = payments[-1].id
+    # will serialize payments into `Payment` objects
+    # and store these in the nosql database
+    return payments
+
+
+app = Stoord()  # fastapi wrapper
+app.add_service(mollie)
+
+app.run_collectors()  # manually run all collectors

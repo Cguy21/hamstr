0ef26d1b5b4b2d8c95f9f4ae79e6f2d692931bfa
Twan <twanwalpot@gmail.com>
2020-10-22 20:07:27 +0200

    collector meta for pipelines

--- hamstr/collectors.py
+++ hamstr/collectors.py
@@ -4,17 +4,41 @@ import httpx
 from pydantic import BaseModel, parse_obj_as
 
 
-class Collector:
+class Pipeline:
+    is_pipe = True
+
+
+def pipeline(obj: Any, *args, **kwargs):
+
+    def decorator(func):
+        pass
+    setattr(decorator, 'is_pipe', True)
+
+    return decorator
+
+
+class CollectorMeta(type):
     
-    def __init__(
-        self,
-        func: Callable[[Any], Any],
-        model: Any,
-        **options: Any
-    ) -> None:
-        self.func = func
-        self.model = model
-        self.callbacks = options.pop('callbacks', [])
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        for n, v in namespace.items():
+            if n == 'test':
+                print(dir(v))
+        cls = super().__new__(mcs, name, bases, namespace)
+        return cls
+
+
+class Collector(metaclass=CollectorMeta):
+    model: Any = None
+
+    def __init__(self):
+        self.callbacks = []
+
+    def collect(self):
+        raise NotImplementedError
+
+    @pipeline
+    def test(self):
+        pass
 
     def run(self) -> List[Any]:
         collected = self.collect()

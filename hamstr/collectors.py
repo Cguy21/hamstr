ece27eefbfcf844485d0a5e58b384241313a1d59
Twan <twanwalpot@gmail.com>
2020-10-22 22:15:34 +0200

    rm httpx import, rm pipeline prototyping

--- hamstr/collectors.py
+++ hamstr/collectors.py
@@ -1,33 +1,9 @@
 from typing import Callable, Any, List, Union
 
-import httpx
 from pydantic import BaseModel, parse_obj_as
 
 
-class Pipeline:
-    is_pipe = True
-
-
-def pipeline(obj: Any, *args, **kwargs):
-
-    def decorator(func):
-        pass
-    setattr(decorator, 'is_pipe', True)
-
-    return decorator
-
-
-class CollectorMeta(type):
-    
-    def __new__(mcs, name, bases, namespace, **kwargs):
-        for n, v in namespace.items():
-            if n == 'test':
-                print(dir(v))
-        cls = super().__new__(mcs, name, bases, namespace)
-        return cls
-
-
-class Collector(metaclass=CollectorMeta):
+class Collector():
     model: Any = None
 
     def __init__(self):
@@ -35,10 +11,6 @@ class Collector(metaclass=CollectorMeta):
 
     def collect(self):
         raise NotImplementedError
-
-    @pipeline
-    def test(self):
-        pass
 
     def run(self) -> List[Any]:
         collected = self.collect()

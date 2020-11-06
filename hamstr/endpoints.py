0ef80e4f8a0f788eea4111f207edfc3078bdd20c
Twan <twanwalpot@gmail.com>
2020-11-06 16:26:58 +0100

    endpoint construction func

--- hamstr/endpoints.py
+++ hamstr/endpoints.py
@@ -1,66 +1,57 @@
 from __future__ import annotations
 from urllib.parse import urljoin
+from dataclasses import dataclass
 
 
 def endpoint(
-    url_or_ending: str,
+    url: str,
+    api_key: str = None,
+    offset_name: str = None,
+    limit_name: str = None,
     service: Service = None,
-    **options
 ):
-    if not service:
-        service = Service(**options)
-    else:
-        service.update(**options)
-    return Endpoint(url_or_ending, service)
+    """
+    Creates Endpoints for collector to use.
+    """
+    # if not service:
+    #     service = Service(**options)
+    # else:
+    #     service.update(**options)
+    # return Endpoint(url_or_ending, service)
+    if service:
+        url = urljoin(service.base_url, url)
+        # function args have higher priority
+        # than service args.
+        api_key = api_key or service.api_key
+        offset_name = offset_name or service.offset_name 
+        limit_name = limit_name or service.limit_name
+    return Endpoint(
+        url,
+        api_key=api_key,
+        offset_name=offset_name,
+        limit_name=limit_name
+    )
 
 
 class Endpoint:
 
-    def __init__(self, url: str, service: Service):
-        self._url = url
-        self.service = service
-
-    @property
-    def url(self):
-        if self.service.base_url:
-            return self.service.get_url(self._url)
-        return self._url
-    
-    @property
-    def api_key(self):
-        return self.service.api_key
-
-
-class Service:
-    """
-    Class to define and easily reuse common data for your endpoints.
-
-    >>> example = Service('https://api.example.com', api_key='test')
-    >>> endpoints = [endpoint('examples', service=example)]
-    """
-
     def __init__(
         self,
-        base_url: str = None,
+        url: str,
+        *,
         api_key: str = None,
         offset_name: str = None,
         limit_name: str = None
     ):
-        self.base_url = base_url
+        self.url = url
         self.api_key = api_key
         self.offset_name = offset_name or 'offset'
         self.limit_name = limit_name or 'limit'
 
-    def update(self, **options):
-        for k, v in options.items():
-            if getattr(self, k, 'not') != 'not':
-                setattr(self, k, v)
 
-    def get_url(self, endpoint: str) -> str:
-        """
-        Returns the result of joining `base_url` and `endpoint`.
-
-        >>> get_full_url('examples')
-        'https://api.example.com/examples'
-        """
-        return urljoin(self.base_url, endpoint)
+@dataclass
+class Service:
+    base_url: str = None
+    api_key: str = None
+    offset_name: str = None
+    limit_name: str = None
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -19,37 +19,3 @@ def test_endpoint_with_service():
 def service() -> Service:
     example = Service('https://api.example.com')
     return example
-
-
-@pytest.mark.parametrize(
-    'base, endpoint, joined', [
-        ('https://api.example.com', 'examples', 'https://api.example.com/examples'),
-        ('https://api.example.com/examples', 'https://api.example.com/examples', 'https://api.example.com/examples')
-    ]
-)
-def test_get_full_url(base, endpoint, joined):
-    service = Service(base)
-    full = service.get_url(endpoint)
-    assert full == joined
-
-
-def test_service_update(service):
-    assert service.base_url == 'https://api.example.com'
-    assert service.api_key == None
-    assert service.offset_name == 'offset'
-    assert service.limit_name == 'limit'
-
-    service.update(
-        api_key='test',
-    )
-    assert service.api_key == 'test'
-
-    with pytest.raises(AttributeError):
-        service.update(nonexist='should raise')
-        service.nonexist
-
-
-def test_endpoint_introspection(service):
-    test_endpoint = Endpoint('examples', service=service)
-    assert test_endpoint.url == service.get_url('examples')
-    assert test_endpoint.api_key == service.api_key

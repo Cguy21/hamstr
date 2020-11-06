ed64799914affd9aea8298e6c8f65efed0c81d32
Twan <twanwalpot@gmail.com>
2020-11-06 16:27:34 +0100

    metaclass collects settings from collector subclass def

--- hamstr/collectors.py
+++ hamstr/collectors.py
@@ -1,11 +1,82 @@
+from copy import copy
+from datetime import timedelta
+
+from pydantic import BaseModel
 
 
 class CollectorMeta(type):
 
     def __new__(mcs, name, bases, namespace, **kwargs):
-        cls = super().__new__(mcs, name, bases, namespace, **kwargs)
+        """
+        Removes Model class definition from collector class
+        and creates actual pydantic BaseModel from it. Model
+        is not accessible as model attribute. Also, collects
+        all settings and stores them in settings dict attr.
+        """
+        model = namespace.get('Model')
+        if model:
+            # creates pydantic model subclass
+            d = {k: v for k, v in model.__dict__.items() if k != '__dict__'}
+            model = type('Model', (BaseModel,), d)
+
+        ignore = ['Model']
+
+        settings = {}
+        if bases:
+            possible = bases[0].default_settings
+            for k, v in namespace.items():
+                k = k.upper()
+                if k in possible:
+                    settings[k] = v
+            for k in possible:
+                ignore.append(k.lower())
+
+        new_namespace = {
+            'model': model,
+            'settings': settings,
+            **{k: v for k, v in namespace.items() if k not in ignore},
+        }
+        cls = super().__new__(mcs, name, bases, new_namespace, **kwargs)
         return cls
 
 
 class Collector(metaclass=CollectorMeta):
-    pass
+
+    default_settings = {
+        'INTERVAL': timedelta(days=1),
+    }
+
+    def __init__(
+        self,
+        cli_settings: dict = None,
+        project_settings: dict = None,
+    ):
+        self._resolve_settings(cli_settings or {}, project_settings or {})
+
+    def _resolve_settings(self, cli_settings: dict, project_settings: dict) -> dict:
+        """
+        Setting resolution order lowest to highest priority.
+
+            1. Default settings
+            2. (Possible) project settings
+            3. Collector class settings
+            4. (possible) CLI variables
+        """
+        settings = copy(self.default_settings)
+        # project = overwrite_settings(default, project_settings)
+        settings.update(project_settings)
+        settings.update(self.settings)
+        settings.update(cli_settings)
+        self.settings = settings
+
+    def parse_json_response(self, json: dict):
+        """
+        Extracts the model(s) from the json response of the list API call.
+        """
+        # if not self.model:
+        #     return json
+
+    def export_item(self, item: any):
+        """
+        Sends the item to storage backend.
+        """
--- tests/test_collectors.py
+++ tests/test_collectors.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import BaseModel
 
 from hamstr.collectors import Collector
 from hamstr.endpoints import endpoint, Endpoint
@@ -8,6 +9,33 @@ from hamstr.endpoints import endpoint, Endpoint
 def collector():
 
     class BaseCollector(Collector):
+        interval = 3600
         endpoints = [endpoint('https://api.example.com')]
 
+        class Model:
+            a: int
+
     return BaseCollector
+
+
+def test_collector_construction(collector):
+    with pytest.raises(AttributeError):
+        collector.Model
+
+    assert collector.model
+
+    settings = {
+        'INTERVAL': 3600
+    }
+
+    assert collector.settings == settings
+    with pytest.raises(AttributeError):
+        collector.interval
+
+
+def test_collector_settings(collector):
+    project_settings = {}
+    cli_settings = {}
+    instance = collector()
+
+    assert instance.settings == collector.settings
